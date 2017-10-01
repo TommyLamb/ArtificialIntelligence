96d6cc384f8785045f233670a177e406111eaca8
Tommy Lamb <james003pond@gmail.com>
2017-10-01 22:34:50 +0100

    Fixed "missing main" error
    Yes; we all missed it.

--- AStarSearch/src/AStarSearch.java
+++ AStarSearch/src/AStarSearch.java
@@ -4,7 +4,7 @@
 public class AStarSearch {
 
 	
-	public static void main(int arg, String[] args){
+	public static void main(String[] args){
 		AStarSearchAlgorithm search = new AStarSearchAlgorithm(new Node(1, 0, 0, 0, RobotLocation.TRUCK, 0, null));
 		search.startSearch();
 	}
